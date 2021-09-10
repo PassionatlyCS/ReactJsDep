e6bc919a73a6749a642d8a94d108b63c65a402d8
PassionatlyCS <bokharisahab@gmail.com>
2021-09-10 17:35:43 +0500

    test-commit https://github.com/PassionatlyCS/ReactJS_Basics-Deployment

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './components/App'
+
+ReactDOM.render(
+    <App />, document.querySelector('#root')
+)
+
+
+//AIzaSyBrtwpzhC6lFc2lQ1pFx0X6rODzMw-Odjs
